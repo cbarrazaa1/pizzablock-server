d5834c024b71251c0eb2e3f3417a3e685eefa23f
Cesar Barraza <cbarrazaa1@gmail.com>
2020-04-03 20:25:05 -0600

    socket.io setup

--- src/App.ts
+++ src/App.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import http from 'http';
+import io from 'socket.io';
 
 const app = express();
+const httpServer = http.createServer(app);
+const socketServer = io(httpServer);
 
-app.get('/', (req, res) => {
-  res.send('Hi');
-});
+const port = Number(process.env.PORT) ?? 4000;
+const host = String(process.env.HOST) ?? '0.0.0.0';
 
-app.listen(4000, () => {
-  console.log('Listening on port 4000.');
-});
+httpServer.listen(port, host, () => {
+  console.log(`Started PizzaBlock Server on port ${4000}.`);
+})
