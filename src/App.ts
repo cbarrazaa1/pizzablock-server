a25da8e6448506c00ca1d92c6a21cee8d0ba35b4
Cesar Barraza <cbarrazaa1@gmail.com>
2020-04-03 20:27:44 -0600

    fix converting undefined port or host

--- src/App.ts
+++ src/App.ts
@@ -6,8 +6,8 @@ const app = express();
 const httpServer = http.createServer(app);
 const socketServer = io(httpServer);
 
-const port = Number(process.env.PORT) ?? 4000;
-const host = String(process.env.HOST) ?? '0.0.0.0';
+const port = Number(process.env.PORT ?? 4000);
+const host = String(process.env.HOST ?? '0.0.0.0');
 
 httpServer.listen(port, host, () => {
   console.log(`Started PizzaBlock Server on port ${4000}.`);
