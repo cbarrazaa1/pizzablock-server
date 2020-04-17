a2ce14be105666cb773f534706b43da877084d68
Cesar Barraza <cbarrazaa1@gmail.com>
2020-04-16 19:45:33 -0500

    added cors

--- src/HttpServer.ts
+++ src/HttpServer.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import http from 'http';
+import cors from 'cors';
 
 const expressApp = express();
+expressApp.use(cors());
 export const httpServer = http.createServer(expressApp);
