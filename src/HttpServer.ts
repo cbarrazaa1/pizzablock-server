c31e015948e61037ac6974999f57a879c2d0536b
Cesar Barraza <cbarrazaa1@gmail.com>
2020-04-16 20:15:13 -0500

    fix cors10310231923

--- src/HttpServer.ts
+++ src/HttpServer.ts
@@ -3,5 +3,13 @@ import http from 'http';
 import cors from 'cors';
 
 const expressApp = express();
-expressApp.use(cors({origin: 'http://localhost:3000', credentials: true}));
+expressApp.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  if (req.method === "OPTIONS") {
+    return res.send(204);
+  }
+  next();
+});
 export const httpServer = http.createServer(expressApp);
