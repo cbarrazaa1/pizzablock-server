cd2f05abc20b0d669d3acf8d85c4baac50b79548
Cesar Barraza <cbarrazaa1@gmail.com>
2020-04-16 20:25:51 -0500

    fix cors10239120391

--- src/HttpServer.ts
+++ src/HttpServer.ts
@@ -4,9 +4,10 @@ import cors from 'cors';
 
 const expressApp = express();
 expressApp.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.header('Access-Control-Allow-Credentials', 'true');
   if (req.method === "OPTIONS") {
     return res.send(204);
   }
