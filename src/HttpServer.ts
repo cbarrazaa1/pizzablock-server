38a2cf95b32a9822430b34ba07d5a10b0bbabcd0
Cesar Barraza <cbarrazaa1@gmail.com>
2020-04-16 20:33:19 -0500

    test

--- src/HttpServer.ts
+++ src/HttpServer.ts
@@ -3,14 +3,4 @@ import http from 'http';
 import cors from 'cors';
 
 const expressApp = express();
-expressApp.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  res.header('Access-Control-Allow-Credentials', 'true');
-  if (req.method === "OPTIONS") {
-    return res.send(204);
-  }
-  next();
-});
 export const httpServer = http.createServer(expressApp);
