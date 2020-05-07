b874172cde464728241403ae1eb8923d42e99967
Cesar Barraza <cbarrazaa1@gmail.com>
2020-05-07 16:33:43 -0500

    set start level to 5 in multiplayer

--- src/Server.ts
+++ src/Server.ts
@@ -140,15 +140,15 @@ export default class Server {
       const other = this.queue1v1.shift()!;
 
       // notify players that game started
-      this.sendEnterGame1v1(socket, other, 9);
-      this.sendEnterGame1v1(other.socket, thisOne, 9);
+      this.sendEnterGame1v1(socket, other, 0);
+      this.sendEnterGame1v1(other.socket, thisOne, 0);
 
       // create the game
       const gameID = await GameController.createGame({
         mode_id: '5eab7d278c3f100017bdcbb1',
         money_pool: 0,
       });
-      const game = new Game(gameID, [thisOne, other], 9);
+      const game = new Game(gameID, [thisOne, other], 0);
       this.games.push(game);
       return;
     }
@@ -179,14 +179,14 @@ export default class Server {
       }
 
       // notify players
-      this.sendEnterGame1v4(players, 9);
+      this.sendEnterGame1v4(players, 0);
 
       // create the game
       const gameID = await GameController.createGame({
         mode_id: '5eab7d718c3f100017bdcbb2',
         money_pool: 0,
       });
-      const game = new Game(gameID, players, 9);
+      const game = new Game(gameID, players, 0);
       this.games.push(game);
       return;
     }
